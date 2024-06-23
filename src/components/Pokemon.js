517992b94dd8edc9608668a41d5da6a0dd22cdd0
stepweaver <weaverst@mail.gvsu.edu>
2024-06-23 13:36:07 -0400

    Organize layout into front and back of cards

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,32 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Pokemon({ pokemon }) {
+  const [isFront, setIsFront] = useState(true);
+
+  const toggleCard = () => setIsFront(!isFront);
+
   return (
-    <div>
-      <h1>{pokemon.name}</h1>
-      <h2>#{pokemon.id.toString().padStart(4, '0')}</h2>
-      <img
-        src={pokemon.sprites.other.dream_world.front_default}
-        alt={pokemon.name}
-      />
-      <p>Height: {pokemon.height}</p>
-      <p>Weight: {pokemon.weight}</p>
-      <div>
-        <h2>Type</h2>
-        {pokemon.types.map((type, index) => (
-          <p key={index}>{type.type.name}</p>
-        ))}
-        <h2>Stats</h2>
-        {pokemon.stats.map((stat, index) => (
-          <p key={index}>
-            {stat.stat.name}: {stat.base_stat}
-          </p>
-        ))}
-        <h2>Abilities</h2>
-        {pokemon.abilities.map((ability, index) => (
-          <p key={index}>{ability.ability.name}</p>
-        ))}
-      </div>
+    <div className='pokemon-card' onClick={toggleCard}>
+      {isFront ? (
+        <div className='card-front'>
+          <img
+            src={pokemon.sprites?.other?.dream_world?.front_default || ''}
+            alt={pokemon.name}
+          />
+          <div className='card-info'>
+            <h2>#{pokemon.id?.toString().padStart(4, '0')}</h2>
+            <h1>{pokemon.name}</h1>
+            <div className='pokemon-type'>
+              {pokemon.types?.map((type, index) => (
+                <p key={index}>{type.type.name}</p>
+              ))}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className='card-back'>
+          <div className='pokemon-stats'>
+            {pokemon.stats?.map((stat, index) => (
+              <p key={index}>
+                {stat.stat.name}: {stat.base_stat}
+              </p>
+            ))}
+          </div>
+          <div className='pokemon-abilities'>
+            {pokemon.abilities?.map((ability, index) => (
+              <p key={index}>{ability.ability.name}</p>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
